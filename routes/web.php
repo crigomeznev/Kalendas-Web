a01e2a9beff06ae1139fe243296f39b9b2e156d1
cristian <cristian>
2022-06-02 15:33:23 +0200

    google

--- routes/web.php
+++ routes/web.php
@@ -77,6 +77,21 @@ Route::post('/calendars/date', [CalendarController::class, 'date'])
     
 
 Route::get('/google', function(){
+    $event = new Event;
+
+    $event->name = 'A new event';
+    $event->description = 'Event description';
+    $event->startDateTime = Carbon\Carbon::now();
+    $event->endDateTime = Carbon\Carbon::now()->addHour();
+    $event->addAttendee([
+        'email' => 'john@example.com',
+        'name' => 'John Doe',
+        'comment' => 'Lorum ipsum',
+    ]);
+    $event->addAttendee(['email' => 'anotherEmail@gmail.com']);
+    
+    $event->save();
+
     $events = Event::get();
     var_dump($events);
 });
