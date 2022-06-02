ebfb2af08409c506f2f8a1854550440eed7f7ad3
crigomeznev <crigomeznev@gmail.com>
2022-06-02 11:48:19 +0200

    a

--- resources/views/email/verify.blade.php
+++ resources/views/email/verify.blade.php
@@ -10,7 +10,5 @@
     <h2>Kalendas - Verify email</h2>
     <p>You successfully registered in Kalendas! There's only one step left, you need to verify your email address</p>
     <a href="{{$url}}">Click here to validate your account</a>
-
-    <img src="{{ $message->embed(asset('/img/logo.png')) }}">
 </body>
 </html>
